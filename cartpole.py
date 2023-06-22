760361f46b10aab3b81dedd040125e3be57d605d
Charles Wang <thewangclass@gmail.com>
2023-06-22 14:01:52 -0500

    split dynamics into x_acc and theta_acc

--- cartpole.py
+++ cartpole.py
@@ -29,11 +29,12 @@ class CartPole():
         self.mu_pole = 0    # coeff friction of pole
         self.force_normal_cart = 1
 
+        # acceleration calculations
+        self.theta_acc = 0
+        self.x_acc = 0
 
         # metadata lists "render_fps" as 50. This is where the tau value of 0.02 comes from because 50 frames per second results in 1/50 which is 0.02 seconds per frame.
-        self.tau = 0.02  # seconds between state updates, our delta_t
-
-
+        self.tau = 1  # seconds between state updates, our delta_t
 
         self.kinematics_integrator = "rk4"  # we use rk4 for our integration
 
@@ -83,7 +84,6 @@ class CartPole():
         high = 0.05    
         low = -high     
         self.state = np.random.uniform(low=low, high=high, size=(4,)).astype(np.float32)
-
         return np.array(self.state, dtype=np.float32)
 
 
@@ -92,7 +92,24 @@ class CartPole():
         assert action in self.action_space, f"invalid action chosen: {action}"
         assert self.state is not None, "Call reset before step"
 
-        dynamics_function = self.dynamics(action)
+        force = self.force_mag if action == 1 else -self.force_mag
+        theta_acc = self.calc_theta_acc(force)
+        x_acc = self.calc_x_acc(force)
+        
+
+        # current state is comprised of x, x_dot, theta, theta_dot
+        # change in each of these is x_dot, x_acc, theta_dot, theta_acc
+        # call rk4 integration to numerically integrate and approximate solutions
+        print("Initial x position: {}".format(self.state[0]))
+        print("Initial x velocity: {}".format(self.state[1]))
+        print("Initial x acc = {}".format(x_acc))
+        change_in_x = rk4(self.calc_x_vel, self.state[1], force, self.tau)
+        # change_in_x_dot = rk4(self.calc_x_acc, self, force, self.tau)
+        # change_in_theta = rk4(self.calc_theta_vel, self, force, self.tau)
+        # change_in_theta_dot = rk4(self.calc_theta_acc, self, force, self.tau)
+        
+        print("x velocity approx by rk4: {}".format(change_in_x))
+
 
         # # Implement rk4 integration using https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
         # # scipy.integrate.RK45
@@ -103,29 +120,20 @@ class CartPole():
         # k4 = x_dot + self.tau*k3            # slope at end of interval
         # x = x + self.tau/6 * (k1 + 2*(k2 + k3) + k4)
 
-    
-    def dynamics(self, action):
-         # Get position, velocity, angle, and angular velocity from state
+    def calc_x_vel(self, x_dot, force):
+        return x_dot + self.tau*self.x_acc
+        pass
+
+
+    def calc_theta_vel(self, state, force):
         x, x_dot, theta, theta_dot = self.state
+        return theta_dot + self.tau*self.theta_acc
 
-        print(self.state)
-        print(type(self.state))
-
-        # If action is 1, then move right, else we use negative force to the left
-        force = self.force_mag if action == 1 else -self.force_mag
+    def calc_theta_acc(self, force):
+        # Get position, velocity, angle, and angular velocity from state
+        x, x_dot, theta, theta_dot = self.state
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
-
-        # Calculate theta_acceleration and x_acceleration
-        temp = (
-            force + self.polemass_length * theta_dot**2 * sintheta
-        ) / self.mass_total
-
-        theta_acc = (self.gravity * sintheta - costheta * temp) / (
-            self.length * (4.0 / 3.0 - self.masspole * costheta**2 / self.mass_total)
-        )
-        x_acc = temp - self.polemass_length * theta_acc * costheta / self.mass_total
-
 
         # Calculate new theta_acc, then normal force of cart, then x_acc according to https://coneural.org/florian/papers/05_cart_pole.pdf
         force_normal_cart = self.force_normal_cart
@@ -148,7 +156,8 @@ class CartPole():
 
         # if normal force changes sign, then compute theta_acc taking into account the new sign
         if get_sign(force_normal_cart) != get_sign(self.force_normal_cart):
-            self.force_normal_cart = force_normal_cart  # update sign of normal force
+            self.force_normal_cart = force_normal_cart      # update sign of normal force
+
             theta_acc = (self.gravity*sintheta * costheta*(
             (
                 (-force - self.masspole*self.length*theta_dot**22*(
@@ -162,21 +171,25 @@ class CartPole():
                                         (costheta - self.mu_cart*get_sign(force_normal_cart*x_dot))
                                     )
                                 )
+        self.theta_acc = theta_acc
+        return theta_acc
 
-        x_acc = (force + self.masspole * self.length * (theta_dot**2 * sintheta - theta_acc * costheta) - (self.mu_cart * force_normal_cart * get_sign(force_normal_cart * x_dot))) / self.mass_total
+    def calc_x_acc(self, force):
+        # Get position, velocity, angle, and angular velocity from state
+        x, x_dot, theta, theta_dot = self.state
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
 
+        x_acc = (force + self.masspole * self.length * (theta_dot**2 * sintheta - self.theta_acc * costheta) - (self.mu_cart * self.force_normal_cart * get_sign(self.force_normal_cart * x_dot))) / self.mass_total
 
-
-
-
-
-
-
-    
+        self.x_acc = x_acc
+        return x_acc
+        
              
 
 if __name__ == '__main__':
     cart = CartPole()
     print(cart.state)
     print(cart.reset())
+    print("testing")
     cart.step(1)

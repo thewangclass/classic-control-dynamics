353548b96f55490e8e6a0a054bc333ee1ab7f1eb
Charles Wang <thewangclass@gmail.com>
2023-06-23 07:35:30 -0500

    added comments to describe cartpole

--- cartpole.py
+++ cartpole.py
@@ -12,7 +12,40 @@ from utils import runge_kutta as rk4
 
 class CartPole():
     """
+    ## Description
+
+    This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson in ["Neuronlike Adaptive Elements That Can Solve Difficult Learning Control Problem"](https://ieeexplore.ieee.org/document/6313077).
+    A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track.
+    The pendulum is placed upright on the cart and the goal is to balance the pole by applying forces in the left and right direction on the cart.
+
+    Our implementation captures the full dynamical system as described in ["Correct equations for the dynamics of the cart-pole system](https://coneural.org/florian/papers/05_cart_pole.pdf). Notably, it allows for the possibility of accounting for frictional forces.
+
+    ## Action Space
+
+    The action is a python set which can take values `{0, 1}` indicating the direction of the fixed force the cart is pushed with.
+
+    - 0: Push cart to the left
+    - 1: Push cart to the right
+
+    **Note**: The velocity that is reduced or increased by the applied force is not fixed and it depends on the angle the pole is pointing. The center of gravity of the pole varies the amount of energy needed to move the cart underneath it.
+
+    ## Observation Space
+    The bounds observation is a `ndarray` with shape `(4,)` with the values corresponding to the following positions and velocities:
+
+    | Num | Observation           | Min                 | Max               |
+    |-----|-----------------------|---------------------|-------------------|
+    | 0   | Cart Position         | -4.8                | 4.8               |
+    | 1   | Cart Velocity         | -Inf                | Inf               |
+    | 2   | Pole Angle            | ~ -0.418 rad (-24°) | ~ 0.418 rad (24°) |
+    | 3   | Pole Angular Velocity | -Inf                | Inf               |
     
+    ## Episode End
+
+    The episode ends if any one of the following occurs:
+
+    1. Termination: Pole Angle is greater than ±12°
+    2. Termination: Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
+    3. Truncation: Episode length is greater than 500
     """
 
 
@@ -50,16 +83,6 @@ class CartPole():
         # 0 push cart to left, 1 push cart to right
         self.action_space = {0, 1}
 
-        """
-        The bounds observation is a `ndarray` with shape `(4,)` with the values corresponding to the following positions and velocities:
-
-        | Num | Observation           | Min                 | Max               |
-        |-----|-----------------------|---------------------|-------------------|
-        | 0   | Cart Position         | -4.8                | 4.8               |
-        | 1   | Cart Velocity         | -Inf                | Inf               |
-        | 2   | Pole Angle            | ~ -0.418 rad (-24°) | ~ 0.418 rad (24°) |
-        | 3   | Pole Angular Velocity | -Inf                | Inf               |
-        """
         # Angle limit set to 2 * theta_threshold_radians so failing observation
         # is still within bounds.
         high = np.array(

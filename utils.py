7b0ce2dbf7a8f6d63608b0d7da4f23e5359a6b5f
Charles Wang <thewangclass@gmail.com>
2023-06-26 10:12:09 -0500

    added wrap and bound from acrobot

--- utils.py
+++ utils.py
@@ -21,3 +21,44 @@ def runge_kutta(dynamics_f, state, action, dt):
     k4 = dynamics_f(state + dt * k3, action)
     next_state = state + (k1 + 2 * k2 + 2 * k3 + k4) * dt / 6
     return next_state
+
+
+
+def wrap(x, m, M):
+    """Wraps ``x`` so m <= x <= M; but unlike ``bound()`` which
+    truncates, ``wrap()`` wraps x around the coordinate system defined by m,M.\n
+    For example, m = -180, M = 180 (degrees), x = 360 --> returns 0.
+
+    Args:
+        x: a scalar
+        m: minimum possible value in range
+        M: maximum possible value in range
+
+    Returns:
+        x: a scalar, wrapped
+    """
+    diff = M - m
+    while x > M:
+        x = x - diff
+    while x < m:
+        x = x + diff
+    return x
+
+
+def bound(x, m, M=None):
+    """Either have m as scalar, so bound(x,m,M) which returns m <= x <= M *OR*
+    have m as length 2 vector, bound(x,m, <IGNORED>) returns m[0] <= x <= m[1].
+
+    Args:
+        x: scalar
+        m: The lower bound
+        M: The upper bound
+
+    Returns:
+        x: scalar, bound between min (m) and Max (M)
+    """
+    if M is None:
+        M = m[1]
+        m = m[0]
+    # bound x between min (m) and Max (M)
+    return min(max(x, m), M)

1c1e673a301d999afb4449db093a98c7a838eb01
thewangclass <thewangclass@gmail.com>
2023-07-11 10:04:50 -0600

    created classic control __init__

--- complete_playground/envs/classic_control/__init__.py
+++ complete_playground/envs/classic_control/__init__.py
@@ -1,2 +1,5 @@
-from complete_playground.envs.acrobot import Acrobot
-from complete_playground.envs.cartpole import CartPole
+from complete_playground.envs.classic_control.acrobot import AcrobotEnv
+from complete_playground.envs.classic_control.cartpole import CartPoleEnv
+from complete_playground.envs.classic_control.mountain_car import MountainCarEnv
+from complete_playground.envs.classic_control.pendulum import PendulumEnv
+from complete_playground.envs.classic_control.quadrotor_2D import Quadrotor2DEnv

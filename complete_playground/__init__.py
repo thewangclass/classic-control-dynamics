eca48fa1e7da0d325e642e03071bea6b0b2c0ee8
thewangclass <thewangclass@gmail.com>
2023-07-11 13:22:22 -0600

    copied complete_playground/__init__.py which contains the portions of code we need

--- complete_playground/__init__.py
+++ complete_playground/__init__.py
@@ -1,1 +1,25 @@
-from complete_playground import envs, rl_training_scripts, utils
+"""Root '__init___' of the complete_playground module setting the '__all__' of complete_playground modules"""
+
+from complete_playground.core import (
+    Env
+)
+from complete_playground.spaces.space import Space
+# from complete_playground.envs.registration import (
+    
+# )
+from complete_playground import envs, spaces, rl_training_scripts, utils, error, logger
+
+__all__ = [
+    # core classes
+    "Env",
+    "Space",
+
+    # registration
+
+    # module folders
+    "envs",
+    "spaces",
+    "utils",
+    "error",
+    "logger",
+]
